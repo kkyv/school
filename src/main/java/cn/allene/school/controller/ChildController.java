1d571c7ced2787804d7a35f47ea2abfac8175e12
OF2898 <869372466@qq.com>
2018-03-05 17:59:10 +0800

    1

--- src/main/java/cn/allene/school/controller/ChildController.java
+++ src/main/java/cn/allene/school/controller/ChildController.java
@@ -6,6 +6,7 @@ import cn.allene.school.po.Child;
 import cn.allene.school.services.ChildService;
 import cn.allene.school.utils.MD5Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +17,7 @@ import java.util.List;
 public class ChildController extends BaseController<Child, String, ChildCondition, ChildService>{
 
     @RequestMapping("/login")
-    public String login(Child child) throws SchoolException {
+    public String login(Model model, Child child) throws SchoolException {
         ChildCondition childCondition = new ChildCondition();
         childCondition.setId(child.getId());
         childCondition.setPassword(MD5Utils.MD5(child.getPassword()));
@@ -24,6 +25,7 @@ public class ChildController extends BaseController<Child, String, ChildConditio
         if(CollectionUtils.isEmpty(childList)){
             throw new SchoolException("用户名或密码错误");
         }
+        model.addAttribute("msg", "登陆成功");
         return "login";
     }
 
